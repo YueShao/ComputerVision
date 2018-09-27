c8527ea49a2733e15c307fe6b3ea970b7f5b542e
YueShao <shaoyue1993@163.com>
2018-09-27 18:18:00 +0800

    ImageEntropy update

--- ImageEntropy.py
+++ ImageEntropy.py
@@ -4,9 +4,10 @@ import numpy as np
 def entropy(img):
 	hist,_=np.histogram(img)
 	hist=hist[hist>0]
-	entr=-np.log2(np.float64(hist)/hist.sum()).sum()
+	p=np.float64(hist)/hist.sum()
+	entr=-(p*np.log2(p)).sum()
 	return entr
 
 if __name__=='__main__':
-	img=cv2.imread("images/table.png")
+	img=cv2.imread("images/tableContrastEnhance.jpg",0)
 	print(entropy(img))
